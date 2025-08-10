f6fdbd13458b417d4c0a7cda192ceee78bd0ca93
Tahsin0077 <a1955229@adelaide.edu.au>
2025-08-10 17:15:37 +0930

    Done7

--- Computer.cpp
+++ Computer.cpp
@@ -19,16 +19,5 @@ Computer::Computer(string n) : name(n) {
 }
 
 Move* Computer::makeMove() {
-    int r = rand() % 8;
-    switch (r) {
-        case 0: return new Rock();
-        case 1: return new Paper();
-        case 2: return new Scissors();
-        case 3: return new Monkey();
-        case 4: return new Robot();
-        case 5: return new Pirate();
-        case 6: return new Ninja();
-        case 7: return new Zombie();
-    }
-    return nullptr;
+    return new Rock();
 }
