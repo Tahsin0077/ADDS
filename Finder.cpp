71b1afe7390b0479ed7a97e7a590a936fb8deff7
Tahsin0077 <a1955229@adelaide.edu.au>
2025-08-31 19:54:03 +0930

    done

---
+++ Finder.cpp
@@ -0,0 +1,24 @@
+#include "Finder.h"
+using namespace std;
+
+vector<int> Finder::findSubstrings(string s1, string s2) {
+    vector<int> result;
+    size_t index = 0;
+
+    for (size_t i = 1; i <= s2.size(); i++) {
+        string prefix = s2.substr(0, i);
+        size_t found = s1.find(prefix, index);
+
+        if (found != string::npos) {
+            result.push_back(found);
+            index = found;
+        } else {
+            for (size_t j = i; j <= s2.size(); j++) {
+                result.push_back(-1);
+            }
+            break;
+        }
+    }
+
+    return result;
+}
---
+++ Finder.h
@@ -0,0 +1,12 @@
+#ifndef FINDER_H
+#define FINDER_H
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Finder {
+public:
+    std::vector<int> findSubstrings(std::string s1, std::string s2);
+};
+
+#endif
